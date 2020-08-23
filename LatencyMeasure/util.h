db55cbc02c8976c3dddeaf63cb3b5a1a3d3bd977
Christian Lorenz <gromeck@gromeck.de>
2020-08-23 09:13:48 +0200

    changes after auto-formatting

--- LatencyMeasure/util.h
+++ LatencyMeasure/util.h
@@ -1,25 +1,25 @@
 /*
-LatencyMeasure
+  LatencyMeasure
 
-(c) 2020 Christian.Lorenz@gromeck.de
+  (c) 2020 Christian.Lorenz@gromeck.de
 
-util definitions and prototypes
+  util definitions and prototypes
 
 
-This file is part of LatencyMeasure.
+  This file is part of LatencyMeasure.
 
-LatencyMeasure is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+  LatencyMeasure is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-LatencyMeasure is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  LatencyMeasure is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with LatencyMeasure.  If not, see <https://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with LatencyMeasure.  If not, see <https://www.gnu.org/licenses/>.
 
 */
 #ifndef __UTIL_H__
