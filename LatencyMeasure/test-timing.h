ac9a54fc91fb76d067453a887781cccf935802bb
Christian Lorenz <gromeck@gromeck.de>
2020-08-23 09:11:54 +0200

    added define for the number of test passes

--- LatencyMeasure/test-timing.h
+++ LatencyMeasure/test-timing.h
@@ -1,23 +1,23 @@
 /*
   LatencyMeasure
-  
+
   (c) 2020 Christian.Lorenz@gromeck.de
-  
+
   test timing definitions and prototypes
-  
-  
+
+
   This file is part of LatencyMeasure.
-  
+
   LatencyMeasure is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
-  
+
   LatencyMeasure is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with LatencyMeasure.  If not, see <https://www.gnu.org/licenses/>.
 
@@ -32,6 +32,11 @@
    to consider the test passed
 */
 #define MAX_DEVIATION 1
+
+/*
+   the number of test passes
+*/
+#define TEST_PASSES      10
 
 /*
    how long will be keep the test LED on in ms
